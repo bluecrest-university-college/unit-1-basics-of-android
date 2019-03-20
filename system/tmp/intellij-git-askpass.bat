966a6da6813745646dd92bf2bcc189de606dccbc
JacquelineKotey <jacqueline.kotey@bluecrest.com>
2019-03-19 23:12:02 -0700

    this is fun {joel lomotey}

---
+++ system/tmp/intellij-git-askpass.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/Android/Android Studio/jre/jre/bin/java" -cp "C:/Program Files/Android/Android Studio/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/Android/Android Studio/lib/xmlrpc-2.0.1.jar;C:/Program Files/Android/Android Studio/lib/commons-codec-1.10.jar;C:/Program Files/Android/Android Studio/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp %*
---
+++ system/tmp/intellij-git-ssh.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/Android/Android Studio/jre/jre/bin/java" -cp "C:/Program Files/Android/Android Studio/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/Android/Android Studio/lib/xmlrpc-2.0.1.jar;C:/Program Files/Android/Android Studio/lib/commons-codec-1.10.jar;C:/Program Files/Android/Android Studio/lib/util.jar;C:/Program Files/Android/Android Studio/lib/trilead-ssh2-build-217-jenkins-11.jar;C:/Program Files/Android/Android Studio/lib/eddsa-0.2.0.jar;C:/Program Files/Android/Android Studio/lib/jbcrypt-1.0.0.jar" org.jetbrains.git4idea.ssh.SSHMain %*
